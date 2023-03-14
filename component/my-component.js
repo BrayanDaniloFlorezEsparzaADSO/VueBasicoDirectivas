90be483cbf3efe5afa5aa2be08b567de388db9b3
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2023-03-14 17:06:51 -0500

    feat: :sparkles: v-html
    utilizar v-html

--- component/my-component.js
+++ component/my-component.js
@@ -1,9 +1,10 @@
 export default{
+
     data(){
         return{
             basico:`<strong style ="color:green ;"> Apolo</strong>`,
             Intermedio:`<strong style ="color:orange ;"> Artemis</strong>`,
-            avanzado:`<strong style ="color:blue ;"> sputnik</strong>`
+            avanzado:`<strong style ="color:red ;"> sputnik</strong>`
         }
     }
 }

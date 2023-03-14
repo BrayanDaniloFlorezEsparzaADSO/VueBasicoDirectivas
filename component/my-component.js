ace946c662a6fefb9b4124589530bfccbf3715d3
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2023-03-14 16:52:50 -0500

    feat: :rocket: v-text
    utilizar el v-text

---
+++ component/my-component.js
@@ -0,0 +1,9 @@
+export default{
+    data(){
+        return{
+            basico:`<strong style ="color:green ;"> Apolo</strong>`,
+            Intermedio:`<strong style ="color:orange ;"> Artemis</strong>`,
+            avanzado:`<strong style ="color:blue ;"> sputnik</strong>`
+        }
+    }
+}

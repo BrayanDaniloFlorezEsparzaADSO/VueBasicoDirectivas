4b6cbf02c435bf272e7d3b3fa19c4cfc31557a16
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2023-03-20 21:00:03 -0500

    feat: :sparkles: v-bind
    se utilizo la directiva de v-bind

--- component/my-component.js
+++ component/my-component.js
@@ -2,12 +2,8 @@ export default{
 
     data(){
         return{
-            cosa:[
-                "Wuenas",
-                "Hola",
-                "No se que poner",
-            
-            ]
+            imagen:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
+            texto:"mondongo"
         }
     }
 }

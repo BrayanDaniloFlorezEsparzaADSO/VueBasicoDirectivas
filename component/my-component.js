c0ea5fdd4459374771ee9d4381ee3debf4035292
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2023-03-20 20:51:06 -0500

    feat: :sparkles: v-for}
    se utilizo la directiva de v-for

--- component/my-component.js
+++ component/my-component.js
@@ -2,12 +2,12 @@ export default{
 
     data(){
         return{
-            mensaje:`<em>wuenas</em>`,
-            mensaje2:`<mark>hola</mark>`,
-            mensaje3:`<u>no se que poner</u>`,
-            mensaje4:`no cumple condicion`,
-            opcion:null
+            cosa:[
+                "Wuenas",
+                "Hola",
+                "No se que poner",
             
+            ]
         }
     }
 }

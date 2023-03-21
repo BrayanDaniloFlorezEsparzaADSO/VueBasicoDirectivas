d2a963cf103ae5edca36928779226fac7b0db593
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2023-03-21 13:41:46 -0500

    feat: :sparkles: v-bind objetos
    se utilizo la directiva de v-bind objetos

--- component/my-component.js
+++ component/my-component.js
@@ -2,10 +2,11 @@ export default{
 
     data(){
         return{
-            atributo:"src",
-            imagen:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
-            atributo2:"alt",
-            texto:"mondongo"
+            imagen:{
+                src:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
+                title:"mondongo"
+            }
+            
         }
     }
 }

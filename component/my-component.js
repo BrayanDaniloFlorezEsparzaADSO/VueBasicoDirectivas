14dc031bc4c4d467afa988795186b0d7a6dab401
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2023-03-20 20:05:14 -0500

    feat: :sparkles: v-model
    se utilizo la directiva de v-model

--- component/my-component.js
+++ component/my-component.js
@@ -2,9 +2,7 @@ export default{
 
     data(){
         return{
-            basico:`<strong style ="color:green ;"> Apolo</strong>`,
-            Intermedio:`<strong style ="color:orange ;"> Artemis</strong>`,
-            avanzado:`<strong style ="color:red ;"> sputnik</strong>`
+            mensaje:"la queso"  
         }
     }
 }

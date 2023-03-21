22285746a77a70b0091b0d3159441efa8ddd71ca
BrayanDaniloFlorezEsparzaADSO <brayandanilo2004@gmail.com>
2023-03-21 13:48:22 -0500

    feat: :sparkles: v-bind class
    se utilizo la directiva de v-bind class

--- component/my-component.js
+++ component/my-component.js
@@ -2,11 +2,13 @@ export default{
 
     data(){
         return{
-            imagen:{
-                src:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
-                title:"mondongo"
+            className:"box",
+            className2:["box","box2"],
+            className3:{
+                box:true,
+                box2:false,
+                box3:true
             }
-            
         }
     }
 }
